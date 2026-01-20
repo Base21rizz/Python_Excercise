5cd0bb06a28ab2e7035f3b7db8ee1ace790b914b
Base21rizz <shoumikislamofficial@gmail.com>
2026-01-20 21:30:35 +0600

    Unpacking

--- ExceptionHandling.py
+++ ExceptionHandling.py
@@ -1,3 +1,9 @@
+# Functions
+def sum_of_five_nums(a, b, c, d, e):
+    return a+b+c+d+e
+
+
+# Exception Handling
 try:
     name = input('Enter your name: ')
     year_born = input('year you born: ')
@@ -5,3 +11,9 @@ try:
     print(f'You are {name}. And your age is {age}')
 except:
     print(e)
+
+# Unpacking
+
+lst = [1, 2, 3, 4, 5]
+# print(sum_of_five_nums(lst)) error cause the other four wasn't called
+print(sum_of_five_nums(*lst))
